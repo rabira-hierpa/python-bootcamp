e19e243cb844152543f1f1289ef711ebff35e285
Rabra <rabira.hierpa@gmail.com>
2020-07-13 20:32:14 +0300

    Added while loops

---
+++ 3-loops/while_loops.py
@@ -0,0 +1,12 @@
+msg = input("Whats the secret password? ")
+while msg != "bananas":
+    print("WRONG!")
+    msg = input("Whats the secret password?")
+print("CORRECT!!!")
+
+total_run = 0
+while True:
+    print("Loop running")
+    total_run += 1
+else:
+    print("Impossible/Unreachable code")
